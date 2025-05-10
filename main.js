43612a0ce90fb971bf92b25eb9183a7ad41a5635
Dika Afty <dikaafty@gmail.com>
2025-05-10 19:47:13 +0700

    Select important todo list element

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Selectors
 const todayTodoList = document.getElementById('todayTodoList');
 const allTodoList = document.getElementById('allTodoList');
+const importantTodoList = document.getElementById('importantTodoList');
 const addTaskBtn = document.querySelector('.addTaskBtn');
 const dialog = document.querySelector('dialog');
 const cancelBtn = document.querySelector('.cancelBtn');
