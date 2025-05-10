06685b63ccb8acca2229b2bd99fac524c6007258
Dika Afty <dikaafty@gmail.com>
2025-05-10 19:47:41 +0700

    Select completed todo list element

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const todayTodoList = document.getElementById('todayTodoList');
 const allTodoList = document.getElementById('allTodoList');
 const importantTodoList = document.getElementById('importantTodoList');
+const completedTodoList = document.getElementById('completedTodoList'); 
 const addTaskBtn = document.querySelector('.addTaskBtn');
 const dialog = document.querySelector('dialog');
 const cancelBtn = document.querySelector('.cancelBtn');
