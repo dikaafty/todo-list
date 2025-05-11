4a6bf1002e3a432e53dcc2d14c1a8c607a36efb0
Dika Afty <dikaafty@gmail.com>
2025-05-11 11:28:13 +0700

    Select task input element

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const todayTodoList = document.getElementById('todayTodoList');
 const allTodoList = document.getElementById('allTodoList');
 const importantTodoList = document.getElementById('importantTodoList');
 const completedTodoList = document.getElementById('completedTodoList'); 
+const taskInput = document.getElementById('task');
 const addTaskBtn = document.querySelector('.addTaskBtn');
 const dialog = document.querySelector('dialog');
 const cancelBtn = document.querySelector('.cancelBtn');
