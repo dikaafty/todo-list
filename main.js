e5a9c744552766eaf5a99d82f3a778c403b0f94a
Dika Afty <dikaafty@gmail.com>
2025-05-12 18:38:54 +0700

    Select every add task button element

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const taskInput = document.getElementById('task');
 const dateInput = document.getElementById('date');
 const notesInput = document.getElementById('notes');
 const priorityInput = document.getElementsByName('priority');
-
+const addTaskBtns = document.querySelectorAll('.addTaskBtn');
 const dialog = document.querySelector('dialog');
 const cancelBtn = document.querySelector('.cancelBtn');
 const submitBtn = document.querySelector('.submitBtn');
